6c001ee9021ad7d58d3e87e3e057401cf86539d9
yosi77774 <yosi77774@gmail.com>
2024-12-25 14:59:20 +0200

    Update .gitignore to exclude node_modules, build, and sensitive files

--- src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { auth } from "../firebase";
-
-function PrivateRoute({ children }) {
-  return auth.currentUser ? children : <Navigate to="/login" />;
-}
-
-export default PrivateRoute;
